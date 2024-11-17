612638736fdb149dc19b76572fac3aa26c9423e8
j3r1ch0123 <110147529+j3r1ch0123@users.noreply.github.com>
2024-11-17 13:42:41 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+curl -fsSL https://ollama.com/install.sh | sh
+ollama pull llama3.2
+ollama pull dolphin-mistral
+ollama pull fixt/home-3b-v3
+ollama serve
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
